7c1d0bbf7c3a9db64dde9d8f24546b03ae22ec0a
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-08-12 19:42:09 +0200

    add increment score and some chances in the visual

--- src/pages/PageExams.vue
+++ src/pages/PageExams.vue
@@ -1,35 +1,32 @@
 <template>
-  <q-page class="">
-    <div
-      v-for="(element, index) in questions.slice(questionCounter,questionCounter1)"
-      :key="index"
-    >
-      <div class="text-center questions">
-        <h6>{{ element.question }}</h6>
+  <div class="container-app">
+    <div class="container-quiz">
+      <div
+        v-for="(element, index) in questions.slice(a,b)"
+        :key="index"
+        class="box"
+      >
+        <div class="box-question">
+          <h6>{{ element.question }}</h6>
+        </div>
+        <div class="box-choices">
+          <q-btn
+            v-for="(item, index) in element.choices"
+            :key="index"
+            :label="item.choice"
+            :class="selected ? verify(item): ''"
+            @click="selectAnswer(item)"
+          />
+        </div>
       </div>
-    <div
-      v-for="(elementAnswers, index) in element.choices"
-      :key="index"
-      class=" q-pa-lg answers flex column flex-center"
-    >
-    <p class="choice-prefix">A</p>
-      <q-btn
-        data-number="1"
-        class=" choice-container"
-        outline
-        rounded
-        color="primary"
-        :label="elementAnswers.choice"
-      />
     </div>
-    </div>
-  </q-page>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
+    return {
       questions:[
         {
           question: 'Inside which HTML element do we put the JavaScript??',
@@ -59,92 +56,42 @@ export default {
           ]
         },
       ],
-      currentQuestion : {},
-      acceptingAnswers : false,
-      score : 0,
-      questionCounter : 0,
-      questionCounter1 : 1,
-      availableQuestions : []
+      a:0,
+      b:1,
+      selected: false,
+      score:0
     }
-  },
-  computed:{
-    // getNewQuestion1() {
-    //   this.questionCounter++
-
-    //   this.availableQuestions = [...this.questions]
-    //   const questionIndex = Math.floor(Math.random() * this.availableQuestions.length)
-    //   this.currentQuestion = this.availableQuestions[questionIndex]
-    //   console.log('question::',this.currentQuestion.question)
-      // return this.currentQuestion.question
-    // },
-    // getNewAnswers() {
-    //   // console.log('question:::',this.getNewQuestion1)
-    //   console.log('::',this.questions.keys)
-    // }
   },
   methods: {
-    getNewQuestion() {
-      this.availableQuestions = [...this.questions]
-
-      this.questionCounter++
-      const questionIndex = Math.floor(Math.random() * this.availableQuestions.length)
-      this.currentQuestion = this.availableQuestions[questionIndex]
-      question.innerText = this.currentQuestion.question
-
-
-      choices.forEach( choice => {
-        const number = choice.dataset['number']
-        choice.innerText = this.currentQuestion['choice' + number]
-
-        this.availableQuestions.slice(questionIndex, 1)
-
-        this.acceptingAnswers = true
-      })
+    incrementScore(choice){
+      choice.answer ? this.score++ : ''
     },
-    verifyIsAnswer(){
-      choices.forEach(choice => {
-        choice.addEventListener('click', e => {
-          if(!this.acceptingAnswers) return
 
-          this.acceptingAnswers = false
-          const selectedChoice = e.target;
-          const selectedAnswer = selectedChoice.dataset['number']
-
-          const classToApply = selectedAnswer == this.currentQuestion.answer
-
-          if (classToApply === 'correct') {
-            this.incrementScore(CORRECT_BONUS)
-          }
-
-          selectedChoice.parentElement.classList.add(classToApply)
-
-          setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-          }, 1000)
-        })
-      })
+    selectAnswer(choice){
+      this.selected = true
+      this.incrementScore(choice)
+      setTimeout(() => {
+        this.nextQuestion()
+      }, 1000)
+      console.log('score::',this.score)
     },
-    incrementScore(num) {
-      score += num;
-    }
+    verify(answer){
+      return answer.answer ? 'correct' : 'incorrect'
+    },
+    nextQuestion() {
+      this.a++
+      this.b++
+      this.selected = false
+    },
   }
 }
 </script>
 
 <style>
-  .choice-container{
-    width: 90%;
+  .correct{
+    background-color: #9ccc65;
   }
-  .choice-container:hover{
-    cursor: none;
-    box-shadow: 0 0.4rem 1.4rem 0 rgba(255, 0, 0, 0.5);
-    transform: translateY(-0.1rem);
-    transition: transform 150ms;
-  }
-  .choice-prefix {
-    padding: 0.5rem 1.5rem;
-    background-color: red;
-    color: white;
+  .incorrect {
+    background-color: #ef5350;
   }
 </style>
