70fbb4657f2cce73d40a6e9c403589ef6d5b190b
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-09-26 00:12:38 +0200

    customizing the setting page

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,74 @@
+<template>
+    <q-page>
+      <div class="picUpdate row">
+        <div class="col-8">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://scontent.fmpm4-1.fna.fbcdn.net/v/t1.6435-9/96734858_2671339216481401_3707348828546924544_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=BbQlaMc7hxIAX8vEjuW&_nc_ht=scontent.fmpm4-1.fna&oh=3886711320ec9f25752e65b2a70b848d&oe=61737F6B">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Ester Da Graça</q-item-label>
+              <q-item-label caption>Estelinha</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <div class="col-4 row items-end justify-end q-pa-sm q-pr-lg">
+          <q-btn
+            no-caps
+            color="indigo-5"
+            text-color="white"
+            label="Editar "
+          />
+        </div>
+      </div>
+      <div class="updateDatas bg-grey-5 q-ma-md q-mr-lg">
+        <div class="name">
+          <q-item>
+            <q-item-section>
+              <q-item-label caption>Nome</q-item-label>
+              <q-item-label>Ester Da Graça</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <div class="name">
+          <q-item>
+            <q-item-section>
+              <q-item-label caption>Username</q-item-label>
+              <q-item-label>Estelinha</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <div class="name">
+          <q-item>
+            <q-item-section>
+              <q-item-label caption>Email</q-item-label>
+              <q-item-label>esterdagraca@gmail.com</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <div class="name">
+          <q-item>
+            <q-item-section>
+              <q-item-label caption>Password</q-item-label>
+              <q-item-label>************</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+      </div>
+      <!-- <div class="subscription"></div> -->
+
+    </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
