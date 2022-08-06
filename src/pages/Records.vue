25f0f03736695d3d1f63c69f7125312b894f810e
nickmu <58500613+nmmuchate@users.noreply.github.com>
2022-08-06 21:53:31 +0000

    change color and icon

--- src/pages/Records.vue
+++ src/pages/Records.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="px-8 py-6">
+     <h5 class="text-xl font-bold leading-none text-gray-900 text-center pb-3">Classificações</h5>
     <q-tabs
       dense
       class="text-dark"
       active-color="primary"
       indicator-color="primary"
-      align="justify"
+      align="center"
+
     >
         <q-tab>
             <q-card class="my-card">
               <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
               <q-card-actions>
-                <q-btn class="text-center" no-caps flat>Ranking</q-btn>
+                <q-btn
+                dense
+                  class="text-center"
+                  no-caps
+                  flat>Resultados Nacionais</q-btn>
               </q-card-actions>
             </q-card>
         </q-tab>
@@ -19,23 +25,11 @@
           <q-card class="my-card">
             <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
             <q-card-actions>
-              <q-btn class="text-center" no-caps flat>Ranking</q-btn>
-            </q-card-actions>
-          </q-card>
-        </q-tab>
-        <q-tab>
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
-            <q-card-actions>
-              <q-btn class="text-center" no-caps flat>Desafio</q-btn>
-            </q-card-actions>
-          </q-card>
-        </q-tab>
-        <q-tab>
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
-            <q-card-actions>
-              <q-btn class="text-center" no-caps flat>Desafio</q-btn>
+              <q-btn
+                dense
+                class="text-center"
+                no-caps
+                flat>Resultados Pessoais</q-btn>
             </q-card-actions>
           </q-card>
         </q-tab>
@@ -53,18 +47,22 @@
         </q-item-section>
         <q-item-section>
           <q-item-label>{{user.name}}</q-item-label>
-          <!-- <q-item-label caption lines="2">Quimica</q-item-label> -->
+         <q-item-label
+          caption
+          lines="2">
+          {{user.detailsGame.countHowManyTimesPlayed.subjectWithBigScore}}
+        </q-item-label>
         </q-item-section>
 
         <q-item-section side top>
-          <q-item-label caption>1vs1</q-item-label>
-          <div class="text-red">
-            <q-icon name="arrow_downward" />
+          <q-item-label caption>{{user.detailsGame.highScore}}V</q-item-label>
+          <div :class="changeColor(user)">
+            <q-icon :name="changeIcon(user)" />
           </div>
         </q-item-section>
       </q-item>
-
       <q-separator spaced inset />
+
     </q-list>
   </div>
 </template>
@@ -77,10 +75,32 @@
         users: []
       }
     },
+    methods:{
+      changeIcon(usr){
+       // change the icon if the user high score is equal or greater than 15
+        if(usr.detailsGame.highScore >= 15){
+          return 'arrow_upward'
+        }else{
+          return 'arrow_downward'
+        }
+      },
+      changeColor(usr){
+        // change the color if the user high score is equal or greater than 15
+        if(usr.detailsGame.highScore >= 15){
+          return 'text-green'
+        }else{
+          return 'text-red'
+        }
+      }
+    },
     mounted(){
       dbFApp.collection('users').onSnapshot(snapshot => {
-
+        // get all users from firebase firestore
         this.users.push(...snapshot.docs.map(doc => doc.data()))
+        // sort users by high score and get top 10 users
+        return this.users.sort((a, b) => {
+          return b.detailsGame.highScore - a.detailsGame.highScore
+        }).slice(0, 10)
       })
     }
   }
