50d583e81652304e869ff19794db82dfc66ca621
nickmu <58500613+nmmuchate@users.noreply.github.com>
2022-08-29 19:42:05 +0000

    Update ExamsWithoutTime.vue

--- src/pages/ExamsWithoutTime.vue
+++ src/pages/ExamsWithoutTime.vue
@@ -1,118 +1,180 @@
 <template>
   <q-page>
-    <!-- quiz container -->
-        <img src='../assets/abstract.svg' alt='' class='absolute -top-10 left-0 object-none'>
-        <!-- quiz score container -->
-      <div class='relative z-20'>
-        <div class="text-gray-800 text-right">
-          <span class="font-medium">Pontuação:</span>
-          <span class="font-bold text-red-600">{{score}}</span>
-        </div>
+    <img src='../assets/copycopy.svg' alt='' class='absolute -top-25 left-0 object-none'>
 
-        <!-- question container -->
-        <div class="rounded-lg bg-gray-100 p-2 .neumorph-1 text-center font-bold tex-gray-800 mt-8">
-          <div class="bg-white p-5">
-            {{getCurrentQuestion.question}}
+    <div class="relative z-20 px-8 py-6">
+
+      <div class="text-gray-800 text-right">
+        <span class="font-medium">
+          Pontuação:
+        </span>
+        <span class="font-bold text-red-600">
+          {{score}}
+        </span>
+      </div>
+
+      <div class="rounded-lg bg-gray-100 mt-12 p-2 neumorph-1 text-center font-bold text-gray-800 mt-8">
+        <div class="bg-white p-5">
+          {{ currentQuestion.question }}
+        </div>
+      </div>
+
+      <div
+        v-for="(choice, item) in currentQuestion.incorrectAnswer"
+        :key="item"
+        class=""
+      >
+        <div :ref="optionChosen" @click="onOptionClicked(choice, item)" class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative">
+          <div class="bg-blue-500 p-1 transform rotate-45 rounded-md h-7 w-7 text-white font-bold absolute right-0 top-0 shadow-md">
+            <p class="transform -rotate-45">
+              +1
+            </p>
           </div>
-        </div>
 
-        <!-- answers container -->
-        <div class='mt-8'>
+          <div class="rounded-lg font-bold flex p-2">
 
-          <!-- answer container -->
-          <div  v-for='(choice, item) in currentQuestion.incorrectAnswer' :key='item' >
-            <div :ref="optionChosen" @click="onOptionClicked(choice, item)" class='neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative'>
-
-              <div class='bg-blue-500 p-1 transform rotate-45 rounded-md h-7 w-7 text-white font-bold absolute right-0 top-0 shadow-md'>
-                <p class='transform -rotate-45'>+1</p>
-              </div>
-
-              <div class="rounded-lg font-bold flex p-2">
-
-                <!-- answer ID -->
-
-                <div class="p-3 rounded-lg">{{ charIndex(item) }}</div>
-
-                <!-- option text -->
-                <div class="flex items-center pl-6">{{ choice }}</div>
-              </div>
+            <div class="p-3 rounded-lg">
+              {{ charIndex(item) }}
             </div>
-          </div>
-        </div>
-        <!-- progress indicator container -->
-        <div class='mt-8 text-center'>
-          <div class='h-1 w-12 rounded-full bg-gray-800 mx-auto'>
-            <p class='text-gray-800 font-bold'>{{questionCounter}}/{{questions.length}}</p>
+
+            <div class="flex items-center pl-6">
+              {{ choice }}
+            </div>
+
           </div>
         </div>
       </div>
 
+      <div class="mt-8 text-center">
+        <div class="h-1 w-12 rounded-full bg-gray-800 mx-auto">
+          <p class="text-gray-800 font-bold">
+            {{ questionCounter }}/{{ questions.length }}
+          </p>
+        </div>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
 <script>
 
-  import { Loading } from 'quasar'
+  import { Loading } from 'quasar';
+  import { dbFApp } from 'src/boot/firebase';
 
-  import { dbFApp } from '../boot/firebase'
-  // import '../index.css'
-
-  import EndOfQuiz from 'src/components/Quiz/EndOfQuiz'
   export default {
-
     data(){
       return{
-        questions: [],
-        currentQuestion: {},
-        questionCounter: 0,
-        activeQuestion: false,
-        answerChosed: null,
+        canClick: true,
+        endOfQuizOver: false,
         score: 0,
+        questionCounter: 0,
+        questions: [],
+        currentQuestion: {
+          question: '',
+          correctAnswer: '',
+          incorrectAnswer: []
+        },
+        itemsRef: [],
+        subjectf: [
+          'CAT1',
+          'CAT10',
+          'CAT2',
+          'CAT3',
+          'CAT4',
+          'CAT5',
+          'CAT6',
+          'CAT7',
+          'CAT8',
+          'CAT9',
+        ]
       }
     },
-    computed: {
-      getCurrentQuestion(){
-        let question = this.questions[this.questionCounter]
-        console.log('question', question)
-        return question
-      }
-    },
+    methods: {
+      charIndex(i){
+        return String.fromCharCode(65 + i)
+      },
+      loadQuestion(){
+        this.canClick = true
+        if (this.questions.length > this.questionCounter) {
+          this.currentQuestion = this.questions[this.questionCounter]
 
-       mounted(){
+          this.questionCounter++
+        }else{
+          this.endOfQuizOver = true
+        }
+      },
+      optionChosen(element) {
+        if(element){
+          this.itemsRef.push(element)
+        }
+      },
+      clearSelected(divSelected){
+        setTimeout(() => {
+          if(divSelected) {
+            divSelected.classList.remove('option-correct')
+            divSelected.classList.remove('option-wrong')
+            divSelected.classList.add('option-default')
+          }
+          this.loadQuestion()
+        },1000)
+      },
+      onOptionClicked(choice, item){
+        if(this.canClick){
+          const divContainer = this.itemsRef[item]
+
+          if(this.currentQuestion.correctAnswer == choice){
+            this.score ++
+            divContainer.classList.add('option-correct')
+            divContainer.classList.remove('option-default')
+          }else{
+            divContainer.classList.add('option-wrong')
+            divContainer.classList.remove('option-default')
+          }
+          this.canClick = false
+          this.clearSelected(divContainer)
+        }else {
+          console.log('Cant Select Question')
+        }
+      }
+    },
+    mounted(){
       Loading.show({
         message: 'Carregando as questões...'
       })
-      let docRef =dbFApp.collection('QUIZ').doc('CAT1')
+      let docRef =dbFApp.collection('QUIZ').doc(this.subjectsf[this.$route.params.id])
       docRef.get().then((querySnapshot) => {
         if (querySnapshot.exists) {
-            const newQuestions = querySnapshot.data().questions
+          const newQuestions = querySnapshot.data().questions
 
-            newQuestions.map((serverQuestion) => {
-              const arrangedQuestion = {
-                question: serverQuestion.question,
-                choices: '',
-                answer: ''
-              };
+          newQuestions.map((serverQuestion) => {
+            const arrangedQuestion = {
+              question: serverQuestion.question,
+              choices: '',
+              answer: ''
+            };
 
-              const choices = serverQuestion.incorrectAnswer;
+            const choices = serverQuestion.incorrectAnswer;
 
-              arrangedQuestion.answer = Math.floor(Math.random() * 4 + 1);
+            arrangedQuestion.answer = Math.floor(Math.random() * 4 + 1);
 
-              choices.splice(arrangedQuestion.answer - 1, 0, serverQuestion.correctAnswer)
+            choices.splice(arrangedQuestion.answer - 1, 0, serverQuestion.correctAnswer)
 
-              arrangedQuestion.choices = choices;
+            arrangedQuestion.choices = choices;
 
-              return arrangedQuestion;
-            });
-            // console.log('new questions ::' , newQuestions.sort(() => Math.random() - 0.5))
-            newQuestions.sort(() => Math.random() - 0.5)
-            this.questions = newQuestions;
-        } else {
+            return arrangedQuestion;
+          });
+          // console.log('new questions ::' , newQuestions.sort(() => Math.random() - 0.5))
+          newQuestions.sort(() => Math.random() - 0.5)
+          this.questions = newQuestions;
+          this.loadQuestion()
+          Loading.hide()
+        }else{
           console.log('No such a document')
         }
-        }).catch((error) => {
-    console.log("Error getting document:", error);
-});
+      }).catch((error) => {
+        console.log("Error getting document:", error.message);
+      })
     },
   }
 </script>
